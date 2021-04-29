a55c7158029dd9c6f3fc4f8dfc4f3bd33a7eee02
Jonee27 <jonee271@gmail.com>
2021-04-29 10:57:27 +0200

    maybe done?

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,26 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) throws IOException {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+
+        String result = getResultString(contacts);
+        Files.write(Path.of(output), result.getBytes());
+    }
+
+    private String getResultString(Map<String, String> contacts) {
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, String> entry : contacts.entrySet()) {
+            result.append(String.format("%s: %s\n", entry.getKey(), entry.getValue()));
+        }
+        return result.toString();
+    }
+}
